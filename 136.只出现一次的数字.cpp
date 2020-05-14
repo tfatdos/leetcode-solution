51d6135553cb8957f4c56b1a6bc2029260dea985
tfatdos <wuxianjue@gmail.com>
2020-05-14 09:14:12 +0800

    add 136

---
+++ 136.只出现一次的数字.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=cpp
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(auto i : nums)
+            ans^=i;
+        return ans;
+    }
+};
+// @lc code=end
+
